135a6cab2c2a0c1b9cc9476e380e31a3438fd795
PKNU <PKNU@DESKTOP-M6ERIMA>
2024-01-30 10:33:03 +0900

    ㅇㅇㅇ

--- thiisjava/src/ch02/sec06/StringExample.java
+++ thiisjava/src/ch02/sec06/StringExample.java
@@ -6,7 +6,7 @@ public class StringExample {
 		String job = "프로그래머";
 		System.out.println(name);
 		System.out.println(job);
-		
+		   
 		String str = "나는 \"자바\"를 배웁니다.";
 		System.out.println(str);
 		
