347d9b55a6273a95402639de609167369bf9cd45
PKNU <PKNU@DESKTOP-M6ERIMA>
2024-01-30 09:41:11 +0900

    0130

---
+++ thisisjava2/src/com/yoonji/leeyoonji.java
@@ -0,0 +1,12 @@
+package com.yoonji;
+
+/*
+ * 이것은 나의 첫번째 프로그램입니다.
+ */
+public class leeyoonji {						// 이것은 leeyoonji 클래스입니다.
+	
+	public static void main(String[] args) {
+		System.out.println("이윤지");
+	}
+}       
+   
