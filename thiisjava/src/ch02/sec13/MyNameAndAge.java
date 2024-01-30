77904d6c13a95912485666a3a391ae9af236123b
PKNU <PKNU@DESKTOP-M6ERIMA>
2024-01-30 17:02:18 +0900

    0130

--- thiisjava/src/ch02/sec13/MyNameAndAge.java
+++ thiisjava/src/ch02/sec13/MyNameAndAge.java
@@ -6,25 +6,22 @@ public class MyNameAndAge {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("이름: ");
-            String name = scanner.nextLine();
+        System.out.println("이름: ");
+        String name = scanner.nextLine();
+        String strY = scanner.nextLine();
+        int age = Integer.parseInt(strY);
 
-            if (!name.equals("이윤지")) {
-                System.out.println("틀렸습니다.");
-                break;
-            }
- 
-            System.out.println("나이: ");
-            String strY = scanner.nextLine();
-            int age = Integer.parseInt(strY);
-
-            if (age != 28) {
-                System.out.println("틀렸습니다.");
-                break;
-            }
+        if (name.equals("이윤지")) {
+        	System.out.println("나이: ");          
+        } else {
+        	System.out.println("틀렸습니다.");
+        }
+        
+        if(age == 28) {
+        	System.out.println("정답입니다.");
+        } else {
+        	System.out.println("틀렸습니다.");
         }
 
-        System.out.println("정답입니다.");
     }
 }
