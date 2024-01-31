cb5b5228b3edac4eae0373bf819e42d1fb24587a
PKNU <PKNU@DESKTOP-M6ERIMA>
2024-01-31 12:10:27 +0900

    0131

--- thiisjava/src/ch03/sec09/BitShiftExample2.java
+++ thiisjava/src/ch03/sec09/BitShiftExample2.java
@@ -2,7 +2,7 @@ package ch03.sec09;
 
 public class BitShiftExample2 {
 	public static void main(String[] args) {
-		int value = 772;
+		int value = 772; //[00000000]	[00000000]		[00000011]		[00000100]
 		
 		//우측으로 3byte(24bit) 이동하고 끝 1바이트만 읽음: [00000000]
 		byte byte1 = (byte) (value >>> 24);
@@ -23,5 +23,8 @@ public class BitShiftExample2 {
 		byte byte4 = (byte) value;
 		int int4 = Byte.toUnsignedInt(byte4);
 		System.out.println("네 번째 바이트 부호 없는 값: " + int4);
+		
+		byte b = -128;
+		System.out.println(b>>1);
 	}
 }
