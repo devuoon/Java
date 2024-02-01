34fd7f286a5c142ec25a9dfc9b447b59fdb25eef
PKNU <PKNU@DESKTOP-M6ERIMA>
2024-02-01 14:55:55 +0900

    0201

--- thiisjava/src/ch05/sec05/CharAtExample.java
+++ thiisjava/src/ch05/sec05/CharAtExample.java
@@ -2,9 +2,9 @@ package ch05.sec05;
 
 public class CharAtExample {
 	public static void main(String[] args) {
-		String ssn = "9506244230123";
+		String ssn = "9506241230123";
 		 char gender = ssn.charAt(6);
-		
+		 
 		if (gender == '1' || gender == '3') {
 			System.out.println("남자입니다.");
 		} else if (gender == '2' || gender == '4') {
