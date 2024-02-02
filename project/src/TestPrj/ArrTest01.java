9a32698d5fabc2e36d238cf4ec1dcdc6118dae32
PKNU <PKNU@DESKTOP-M6ERIMA>
2024-02-02 11:31:14 +0900

    0202

---
+++ project/src/TestPrj/ArrTest01.java
@@ -0,0 +1,31 @@
+package TestPrj;
+
+public class ArrTest01 {
+	public static void arrPrint(int[] a /*매개변수*/) {	// int타입의 배열을 선언
+		for (int i = 0; i<a.length; i++) {
+			System.out.println("a[" + i + "] :" + a[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		int[] ary = { 10, 20, 30, 40, 50 }; // 배열선언과 동시에 초기화
+
+		for (int i = 0; i <= ary.length; i++) { 	//ary.length : 배열의 길이를 통해 값 구하기
+			// System.out.printf("ary[%d]: %d\n", i,ary[i]); //%d : 정수 형식 표기법
+			System.out.println("ary[" + i + "] : " + ary[i]);
+		}
+
+		int ary1[]; // 배열 선언 (= int[] ary;), 메모리 할당 단계 x
+		ary1 = new int[] { 50, 60, 70, 80, 90, 100 }; // 선언 후 배열 초기화 하기
+
+		for (int i = 0; i <= 5; i++) {
+			System.out.println("ary1[" + i + "] :" + ary1[i]);
+		}
+		
+		/*별도의 클래스에서 메서드 생성 하면 메인에 호출 해야함*/
+		arrPrint(new int[] {1,2,3});		//별도의 배열 객체 생성
+		arrPrint(ary);						//배열 ary을 전달 
+		
+	}
+}
---
+++ project/src/project/CalcTest04.java
@@ -0,0 +1,40 @@
+package project;
+
+import java.util.Scanner;
+
+public class CalcTest04 {
+	public static void plus(int num1, int num2) {
+		System.out.print("num1 + num2 =" + ( num1 + num2));
+	}
+	public static void minus(int num1, int num2) {
+		System.out.print("num1 - num2 =" + ( num1 - num2));
+	}
+	public static void multi(int num1, int num2) {
+		System.out.print("num1 * num2 =" + ( num1 * num2));
+	}
+	public static void div(int num1, int num2) {
+		System.out.print("num1 / num2 =" + ( num1 / num2));
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫 번째 숫자: ");
+		int num1 = sc.nextInt();
+		sc.nextLine();
+		System.out.print("두 번째 숫자: ");
+		int num2 = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("연산자를 입력하세요.");
+		String str1 = sc.nextLine();
+		
+		if(str1.equals("+") ) {
+			plus(num1,num2);
+		} else if(str1.equals("-")) {
+			minus(num1,num2);
+		} else if(str1.equals("*")) {
+			multi(num1,num2);
+		} else if(str1.equals("/")) {
+			div(num1,num2);
+		}
+	}
+}
