3256c8949644985a5bb3dd252519b3e3e41394e6
PKNU <PKNU@DESKTOP-M6ERIMA>
2024-02-02 12:06:37 +0900

    0202

--- thiisjava/src/ch05/sec07/MultidimensionalArrayByValueListExample.java
+++ thiisjava/src/ch05/sec07/MultidimensionalArrayByValueListExample.java
@@ -8,6 +8,11 @@ public class MultidimensionalArrayByValueListExample {
 				{76, 88}
 		};
 		
+		//각 반의 학생 수가 다를 경우 점수 저장을 위한 2차원 배열 생성
+		int[][] englishScores = new int[2][];
+		englishScores[0] = new int[2];
+		englishScores[1] = new int[3];
+		
 		//배열의 길이
 		System.out.println("1차원 배열 길이(반의 수): " + scores.length);
 		System.out.println("2차원 배열 길이(첫 번째 반의 학생 수): " + scores[0].length);
