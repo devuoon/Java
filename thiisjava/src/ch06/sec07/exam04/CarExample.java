614fe3f8f9750d62761ecf7c34c07bebe7e6475b
PKNU <PKNU@DESKTOP-M6ERIMA>
2024-02-05 11:06:01 +0900

    0205

--- thiisjava/src/ch06/sec07/exam04/CarExample.java
+++ thiisjava/src/ch06/sec07/exam04/CarExample.java
@@ -1,7 +1,6 @@
 package ch06.sec07.exam04;
 
 public class CarExample {
-
 	public static void main(String[] args) {
 		Car car1 = new Car();			//생성자 호출
 		System.out.println("car1.company : " + car1.company);
@@ -24,5 +23,4 @@ public class CarExample {
 		System.out.println("car4.color : " + car4.color);
 		System.out.println("car4.maxSpeed : " + car4.maxSpeed);
 	}
-
 }
