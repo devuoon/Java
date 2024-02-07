5662ea4e002f3fe68b824ac05931732f2f169ffb
PKNU <dbswl341@naver.com>
2024-02-07 16:52:33 +0900

    0207

--- thiisjava/src/ch08/sec02/RemoteControlExample.java
+++ thiisjava/src/ch08/sec02/RemoteControlExample.java
@@ -3,8 +3,9 @@ package ch08.sec02;
 public class RemoteControlExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RemoteControl rc;
+		rc = new Television();
+		rc.turnOn();
 	}
 
 }
