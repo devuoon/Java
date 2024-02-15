b22ec9530522a32f43accb0694e464a99fb78f2d
PKNU <dbswl341@naver.com>
2024-02-15 16:02:46 +0900

    0215

---
+++ thiisjava/src/ch14/sec06/exam02/WorkObject.java
@@ -0,0 +1,13 @@
+package ch14.sec06.exam02;
+
+public class WorkObject {
+	public synchronized void methodA() {
+		Thread thread = Thread.currentThread();
+		System.out.println(thread.getName() + ": methodA 작업 실행");
+		notify();
+		try {
+			wait();
+		} catch (InterruptedException e) {
+		}
+	}
+}
