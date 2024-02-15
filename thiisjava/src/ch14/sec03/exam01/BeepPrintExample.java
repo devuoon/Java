4b0227021a3b3c1583be59a3a92ba56759f82f42
PKNU <dbswl341@naver.com>
2024-02-15 13:49:13 +0900

    0215

--- thiisjava/src/ch14/sec03/exam01/BeepPrintExample.java
+++ thiisjava/src/ch14/sec03/exam01/BeepPrintExample.java
@@ -3,7 +3,6 @@ package ch14.sec03.exam01;
 import java.awt.Toolkit;
 
 public class BeepPrintExample {
-
 	public static void main(String[] args) {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		for(int i=0; i<5; i++) {
