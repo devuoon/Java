04d2b0a7ed4f18ad71330bf9fff2ec5f0b69bad0
PKNU <dbswl341@naver.com>
2024-02-16 11:30:48 +0900

    0216

--- project/src/TestThread/ThreadV4.java
+++ project/src/TestThread/ThreadV4.java
@@ -1,35 +1,37 @@
 package TestThread;
 
+//클래스 정의
 class MyClass {
-    private int count;
+    private int count;		//count변수 선언
 
-    public void increment() {
+    public void increment() {		//증가 메서드
         count++;
     }
 
-    public void decrement() {
+    public void decrement() {		//감소 메서드
         count--;
     }
 
-    public void getCount() {
+    public void getCount() {		//현재 count값을 출력하는 메서드
         System.out.println("count = " + count);
     }
 }
 
-class ThreadImpl1 implements Runnable {
-    private MyClass myClass;
+// 클래스 정의
+class ThreadImpl1 implements Runnable {		//Runnable 인터페이스 구현
+    private MyClass myClass;		//생성자를 통해 MyClass 객체를 받아옴
 
     public ThreadImpl1(MyClass myClass) {
         this.myClass = myClass;
     }
 
     @Override
-    public void run() {
+    public void run() {				//MyClass 객체의 증가 메서드 호출하여 count 증가
         myClass.increment();
     }
 }
 
-class ThreadImpl2 implements Runnable {
+class ThreadImpl2 implements Runnable {		//Runnable 인터페이스 구현
     private MyClass myClass;
 
     public ThreadImpl2(MyClass myClass) {
@@ -46,14 +48,29 @@ public class ThreadV4 {
     public static void main(String[] args) throws InterruptedException {
         MyClass myClass = new MyClass();
 
-        Thread thread1 = new Thread(new ThreadImpl1(myClass));
-        Thread thread2 = new Thread(new ThreadImpl2(myClass));
+        // 만번 더하는 스레드
+        Thread threadIncrement = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                myClass.increment();
+            }
+        });
 
-        thread1.start();
-        thread1.join();
-        thread2.start();
-        thread2.join();
+        // 만번 빼는 스레드
+        Thread threadDecrement = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                myClass.decrement();
+            }
+        });
 
+        // 두 스레드 실행
+        threadIncrement.start();
+        threadIncrement.join();  // 첫 번째 스레드가 실행 완료될 때까지 대기
+
+        // 다시 만번 빼는 스레드 실행
+        threadDecrement.start();
+        threadDecrement.join();  // 두 번째 스레드가 실행 완료될 때까지 대기
+
+        // 최종 count 출력
         myClass.getCount();
     }
 }
