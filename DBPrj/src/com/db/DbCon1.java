2246204cf8e224b5b1add9448323b7b100745d42
yoonji <dbswl341@naver.com>
2024-04-09 12:31:33 +0900

    0409

--- DBPrj/src/com/db/DbCon1.java
+++ DBPrj/src/com/db/DbCon1.java
@@ -9,17 +9,18 @@ public class DbCon1 {
 		Connection conn = null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			System.out.printf("JDBC Driver 로드 성공!!\n");
+			System.out.printf("JDBC Driver 로드 성공\n");
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
 			String user = "ADAM";
 			String pw = "1234";
 			conn = DriverManager.getConnection(url, user, pw);
-			System.out.printf("DB 연결 성공!!");
+			System.out.printf("DB 연결 성공\n");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				conn.close();
+				System.out.printf("DB 연결 해제");
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
